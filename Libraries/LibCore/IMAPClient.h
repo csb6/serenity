099f7a864e1b0a1b4a4e3a23eaacb8c07027477a
csb6 <cblakley15@gmail.com>
2020-05-03 10:41:36 -0700

    LibCore: Added basic IMAPClient class that can connect to an IMAP server
    Right now, the IMAPClient class is in LibCore, with the ability to
    connect to an IMAP server using a given IP address and port. While
    it is able to make the initial connection to a server, there seems
    to be some trouble in the TCP library that is causing a seg fault.

---
+++ Libraries/LibCore/IMAPClient.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <LibCore/TCPSocket.h>
+#include <AK/ByteBuffer.h>
+#include <AK/String.h>
+#include <AK/StringBuilder.h>
+#include <AK/Vector.h>
+#include <AK/StringView.h>
+#include <stdio.h>
+
+namespace Core {
+
+enum class IMAPResponsePrefix : char {
+    // Command starts with a '*'
+    Untagged,
+    // Begins with some identifier (e.g. a001)
+    Tagged,
+    // Begins with a '+' (response marked with '+' aren't really commands,
+    // but they are pretty close
+    Continuation
+};
+
+enum class IMAPResponseStatus : char {
+    Ok, // Command succeeded
+    No, // Command failed
+    Bad // Command is unrecognized/has syntax error
+};
+
+enum class IMAPSystemFlag : char {
+    Seen, Answered,
+    Flagged, Deleted,
+    Draft, Recent
+};
+
+enum class IMAPConnectionState : char {
+    // Usually default state; in this state when client hasn't
+    // supplied any credentials yet
+    NotAuthenticated,
+    // User has logged in, but not selected anything;
+    // can't do any message-related operations
+    Authenticated,
+    // User has successfully chosen a mailbox
+    Selected
+};
+
+struct IMAPMessage {
+    // id representing a message; can't change within a session
+    u32 uid;
+     // 1-based index in current mailbox; can change within a session
+    u16 sequence_number;
+    // Standardized flags
+    Vector<IMAPSystemFlag> system_flags;
+    // User-created flags; can't start with '/'
+    Vector<String> keywords;
+    // Date format is undefined in IMAP spec
+    String date;
+    // The header of the message as found in RFC-2822
+    String envelope;
+    // The body of the message as found in MIME-IMB
+    String body;
+    // The content of the message
+    String text;
+};
+
+
+class IMAPClient final : public Core::Object {
+    C_OBJECT(IMAPClient)
+public:
+    IMAPClient(StringView address, int port = 143);
+    virtual ~IMAPClient() override;
+    bool login(StringView username, StringView password);
+    bool select(StringView mailbox);
+private:
+    StringBuilder new_message_id();
+    bool send_command(StringView command);
+
+    RefPtr<Core::TCPSocket> m_socket;
+    int m_message_id = 1;
+    IMAPConnectionState m_state = IMAPConnectionState::NotAuthenticated;
+};
+
+}
